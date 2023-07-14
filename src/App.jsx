9476d410cbc216e5ab3ea07bba03e6243e5dd255
DayOneAI <bambathiam88@gmail.com>
2023-07-14 18:10:01 +0200

    Data_icons_firstPerson_ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,47 @@
+import React from 'react'
+import people from './data'
+import { useState } from 'react'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
+
+// // Révision render
+// const App = () => {
+//   const [id, name, job, image, text] = people
+//   return (
+//     <main>
+//       {people.map((per) => {
+//         return (
+//           <div>
+//             <img src={per.image} alt={per.name} />
+//             <h2>{per.name}</h2>
+//             <h3>{per.job}</h3>
+//             <p>{per.text}</p>
+//           </div>
+//         )
+//       })}
+//     </main>
+//   )
+// }
+
 const App = () => {
-  return <h2>Reviews Starter</h2>
+  // Initialise premier index à zéro
+  const [index, setIndex] = useState(0)
+  const { name, job, image, text } = people[index]
+  // console.log(name)
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+      </article>
+    </main>
+  )
 }
+
 export default App
